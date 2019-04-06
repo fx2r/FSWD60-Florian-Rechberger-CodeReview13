fc31f19a052534f94a60e951a2e7e128f4331c63
Florian Rechberger <florianrechberger@gmx.at>
2019-04-06 04:21:06 +0200

    display all events on index with bootstrap card-columns

--- src/AppBundle/Controller/EventController.php
+++ src/AppBundle/Controller/EventController.php
@@ -1,39 +1,51 @@
 <?php
 namespace AppBundle\Controller;
+use AppBundle\Entity\Todo;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 class EventController extends Controller
 {
-   /**
+  /**
     * @Route("/", name="event_list")
     */
-   public function listAction(Request $request)
-   {
-       return $this->render('events/index.html.twig');
-   }
+  public function listAction(Request $request)
+  {
+    
+    $events = $this->getDoctrine()->getRepository('AppBundle:events')->findAll();
+    return $this->render('events/index.html.twig',
+    array('events'=>$events));
+  }
 
   /**
-    * @Route("/events/create", name="event_create")
-    */
-   public function createAction(Request $request)
-   {
-       return $this->render('events/create.html.twig');
-   }
+  * @Route("/create", name="event_create")
+  */
+  public function createAction(Request $request)
+  {   
+
+    return $this->render('events/create.html.twig');
+  }
 
   /**
-    * @Route("/events/details/{id}", name="event_details")
+    * @Route("/details/{id}", name="event_details")
     */
-   public function detailsAction($id)
-   {
-       return $this->render('events/details.html.twig');
-   }
- 
+  public function detailsAction($id)
+  {
+    return $this->render('events/details.html.twig');
+  }
+
   /**
-    * @Route("/events/edit/{id}", name="event_edit")
+    * @Route("/edit/{id}", name="event_edit")
     */
-   public function editAction($id, Request $request)
-   {
-       return $this->render('events/edit.html.twig');
-   }
+  public function editAction($id, Request $request)
+  {
+    return $this->render('events/edit.html.twig');
+  }
 }
