29e0f3ce0efeb5cd4ec862c050716a8d7722dd9d
Florian Rechberger <florianrechberger@gmx.at>
2019-04-06 15:05:42 +0200

    add view, edit, delete functionalities -make flah messages show in top of page

--- src/AppBundle/Controller/EventController.php
+++ src/AppBundle/Controller/EventController.php
@@ -64,7 +64,7 @@ class EventController extends Controller
     ->add('link', TextType::class, array('attr' => array('class'=> 'form-control', 'style'=>'margin-bottom:15px')))
 
     ->add('type', ChoiceType::class, array('choices'=>array(
-        'Museeum'=>'Museeum',
+        'Museum'=>'Museum',
         'Club'=>'Club',
         'Music'=>'Music',
         'Dancing'=>'Dancing',
@@ -145,7 +145,8 @@ class EventController extends Controller
     */
   public function detailsAction($id)
   {
-    return $this->render('events/details.html.twig');
+    $event = $this->getDoctrine()->getRepository('AppBundle:events')->find($id);
+    return $this->render('events/details.html.twig', array('events' => $event));
   }
 
   /**
@@ -153,6 +154,147 @@ class EventController extends Controller
     */
   public function editAction($id, Request $request)
   {
-    return $this->render('events/edit.html.twig');
+    $event = $this->getDoctrine()->getRepository('AppBundle:events')->find($id);
+
+    $event->setName($event->getName());
+    $event->setFrequency($event->getFrequency());
+    $event->setDate($event->getDate());
+    $event->setTime($event->getTime());
+    $event->setDescription($event->getDescription());
+    $event->setImage($event->getImage());
+    $event->setCapacity($event->getCapacity());
+    $event->setEmail($event->getEmail());
+    $event->setPhone($event->getPhone());
+    $event->setPlace($event->getPlace());
+    $event->setStreet($event->getStreet());
+    $event->setPostalCode($event->getPostalCode());
+    $event->setCity($event->getCity());
+    $event->setCountry($event->getCountry());
+    $event->setLink($event->getLink());
+    $event->setType($event->getType());
+
+    //form
+    $form = $this->createFormBuilder($event)
+
+    ->add('name', TextType::class, array('attr' => array('class'=> 'form-control', 'style'=>'margin-bottom:15px')))
+
+    ->add('frequency', TextType::class, array('attr' => array('class'=> 'form-control', 'style'=>'margin-bottom:15px')))
+
+    ->add('date', DateType::class, array('attr' => array('style'=>'margin-bottom:15px')))
+
+    ->add('time', TimeType::class, array('attr' => array('style'=>'margin-bottom:15px')))
+
+    ->add('description', TextareaType::class, array('attr' => array('class'=> 'form-control', 'style'=>'margin-bottom:15px')))
+
+    ->add('image', TextType::class, array('attr' => array('class'=> 'form-control', 'style'=>'margin-bottom:15px')))
+
+    ->add('capacity', NumberType::class, array('attr' => array('class'=> 'form-control', 'style'=>'margin-bottom:15px')))
+
+    ->add('email', TextType::class, array('attr' => array('class'=> 'form-control', 'style'=>'margin-bottom:15px')))    
+
+    ->add('phone', TextType::class, array('attr' => array('class'=> 'form-control', 'style'=>'margin-bottom:15px')))
+
+    ->add('place', TextType::class, array('attr' => array('class'=> 'form-control', 'style'=>'margin-bottom:15px')))
+
+    ->add('street', TextType::class, array('attr' => array('class'=> 'form-control', 'style'=>'margin-bottom:15px')))
+
+    ->add('postal_code', TextType::class, array('attr' => array('class'=> 'form-control', 'style'=>'margin-bottom:15px')))
+
+    ->add('city', TextType::class, array('attr' => array('class'=> 'form-control', 'style'=>'margin-bottom:15px')))
+
+    ->add('country', TextType::class, array('attr' => array('class'=> 'form-control', 'style'=>'margin-bottom:15px')))
+
+    ->add('link', TextType::class, array('attr' => array('class'=> 'form-control', 'style'=>'margin-bottom:15px')))
+
+    ->add('type', ChoiceType::class, array('choices'=>array(
+        'Museum'=>'Museum',
+        'Club'=>'Club',
+        'Music'=>'Music',
+        'Dancing'=>'Dancing',
+        'Politics'=>'Politics',
+        'Education'=>'Education',
+        'Architecture'=>'Architecture',
+        'Wellness'=>'Wellness',
+        'Sports'=>'Sports',
+        'Martial Arts'=>'Martial Arts',
+        'Theatre'=>'Theatre',
+        'Cultural Exchange'=>'Cultural Exchange',
+        'Travel'=>'Travel',
+        'Charity'=>'Charity'
+        ),
+        'attr' => array(
+        'class'=>'form-control', 'style'=>'margin-botton:15px'
+        )
+      )
+    )
+    ->add('link', TextType::class, array('attr' => array('class'=> 'form-control', 'style'=>'margin-bottom:15px')))
+    ->add('save', SubmitType::class, array('label'=> 'Save', 'attr' => array('class'=> 'btn btn-info mt-3', 'style'=>'margin-bottom:15px')))
+    ->getForm();
+       $form->handleRequest($request);
+
+    if($form->isSubmitted() && $form->isValid()){
+
+      //fetching data
+      $name         = $form['name']->getData();
+      $frequency    = $form['frequency']->getData();
+      $date         = $form['date']->getData();
+      $time         = $form['time']->getData();
+      $description  = $form['description']->getData();
+      $image        = $form['image']->getData();
+      $capacity     = $form['capacity']->getData();
+      $email        = $form['email']->getData();
+      $phone        = $form['phone']->getData();
+      $place        = $form['place']->getData();
+      $street       = $form['street']->getData();
+      $postal_code  = $form['postal_code']->getData();
+      $city         = $form['city']->getData();
+      $country      = $form['country']->getData();
+      $link         = $form['link']->getData();
+      $type         = $form['type']->getData();
+
+      $em = $this->getDoctrine()->getManager();
+      $event = $em->getRepository('AppBundle:events')->find($id);
+
+      //function from our entities, each columns has set function to wich we assign the attained value from the form
+      $event->setName($name);
+      $event->setFrequency($frequency);
+      $event->setDate($date);
+      $event->setTime($time);
+      $event->setDescription($description);
+      $event->setImage($image);
+      $event->setCapacity($capacity);
+      $event->setEmail($email);
+      $event->setPhone($phone);
+      $event->setPlace($place);
+      $event->setStreet($street);
+      $event->setPostalCode($postal_code);
+      $event->setCity($city);
+      $event->setCountry($country);
+      $event->setLink($link);
+      $event->setType($type);
+
+      $em->flush();
+      $this->addFlash(
+              'notice',
+              'Event Added'
+              );
+      return $this->redirectToRoute('event_list');
+    }
+    return $this->render('events/edit.html.twig', array('events' => $event, 'form' => $form->createView()));;
   }
+
+  /**
+    * @Route("/delete/{id}", name="event_delete")
+    */
+   public function deleteAction($id){
+                $em = $this->getDoctrine()->getManager();
+           $event = $em->getRepository('AppBundle:events')->find($id);
+           $em->remove($event);
+            $em->flush();
+           $this->addFlash(
+                   'notice',
+                   'Event Removed'
+                   );
+            return $this->redirectToRoute('event_list');
+   }
 }
